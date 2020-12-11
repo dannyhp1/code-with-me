461da84a274f9ce44ac11a23a97d9d9838e95e50
Danny Pham <dpham760@gmail.com>
2020-12-10 17:23:42 -0800

    Modified single editor to be able to call endpoint to execute code.

--- src/components/SingleEditorWrapper.js
+++ src/components/SingleEditorWrapper.js
@@ -1,20 +1,39 @@
 
 import React, { useState, useEffect } from 'react';
-import Editor from './Editor';
+import axios from 'axios';
 import * as Constants from '../constants/config';
+import Editor from './Editor';
+import { Button } from '@material-ui/core';
+
+const executeUrl = process.env.NODE_ENV === 'production' ? Constants.PROD_EXECUTE_ENDPOINT : Constants.DEV_EXECUTE_ENDPOINT;
+const disableCode = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_USE_AWS === 'false' : false;
 
 function SingleEditorWrapper(props) {
-    const [executingCode, setExecutingCode] = useState(false);
+    const [executingCode, setExecutingCode] = useState(disableCode);
     const [code, setCode] = useState('');
 
     useEffect(() => {
 
     }, []);
 
+    const executeCode = () => {
+        setExecutingCode(true);
+
+        axios.post(executeUrl, { 'code': code })
+            .then(response => {
+                console.log(response);
+                setExecutingCode(false);
+            }).catch(error => {
+                console.log('Failed to execute');
+                setExecutingCode(false);
+            });
+    }
+
     return (
         <div>
             <p>Single Editor Wrapper!</p>
             <Editor source={code} onChange={(message) => setCode(message)} />
+            <Button variant='contained' color='primary' onClick={executeCode} disabled={executingCode}>Run Code</Button>
         </div>
     )
 
