ba97017cf3b517553f805f11abd4ccd4defe8607
Danny Pham <dpham760@gmail.com>
2021-01-03 12:02:57 -0800

    Modified heroku endpoint to new executor server.

--- src/constants/config.js
+++ src/constants/config.js
@@ -11,5 +11,5 @@ export const SOCKET_OPTIONS = { transports: ['websocket'] };
 
 // Endpoints for execution.
 export const DEV_EXECUTE_ENDPOINT = 'http://localhost:8181/execute';
-export const PROD_EXECUTE_HEROKU_ENDPOINT = 'https://code-with-me-phamdann.herokuapp.com/execute';
+export const PROD_EXECUTE_HEROKU_ENDPOINT = 'https://code-with-me-executor-phamdann.herokuapp.com/v1/execute';
 export const PROD_EXECUTE_AWS_ENDPOINT = 'https://aws.dannyhp.com/code-with-me/execute';
